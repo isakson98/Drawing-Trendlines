5747afbd74f47fb392189521a60c9366fe6f5fca
Egor <isakson98@gmail.com>
2021-07-09 02:29:44 -0400

    adding high/low to filter

--- LinearReg_Trendline.py
+++ LinearReg_Trendline.py
@@ -12,6 +12,46 @@ import datetime as dt
 # data extraction
 from DataExtract import retrieve_ticker_data
 
+# to perform linear regression well (meaning draw a clear trendline) 
+# more than once on one chart, I need to be able to assign starting points
+# to the lines and end points.
+# Starting points brainstorm:
+# -- higher highs / lower highs (easiest)
+# -- 
+def identify_lows_highs(ohlc, ohlc_type, len, isHigh):
+
+    ohlc[f"{ohlc_type} Extremes"] = False
+
+    for i, r in ohlc.iterrows():
+
+        isFound = True
+        current_price = ohlc.at[i, ohlc_type]
+
+        ohlc_fragment = ohlc[i - dt.timedelta(days=len) : i]
+        prev_extreme = ohlc_fragment[ohlc_type].rolling(window=len, min_periods=1).max()       
+        prev_extreme = prev_extreme[i]
+        print(prev_extreme)
+        if isHigh and (prev_extreme > current_price):
+            isFound = False
+        if not isHigh and (prev_extreme < current_price):
+            isFound = False
+
+
+        ohlc_fragment = ohlc[i - dt.timedelta(days=len) * 2 : i - dt.timedelta(days=len)]
+        prev_extreme = ohlc_fragment[ohlc_type].rolling(window=len, min_periods=1).max()
+        print(prev_extreme)
+        prev_extreme = prev_extreme[i - dt.timedelta(days=len) * 2]
+        if isHigh and (prev_extreme >= current_price):
+            isFound = False
+        if not isHigh and (prev_extreme <= current_price):
+            isFound = False    
+        
+        if isFound:
+            ohlc.at[i, f"{ohlc_type} Extremes"] = True
+            print(i)
+
+    pass
+
 
 # allows to specify which range of values you want a trendline to be drawn at
 # calculating trendlines based on the price candles
@@ -51,13 +91,17 @@ def identify_trendlines_LinReg(ohlc_data, start=None, end=None):
         ohlc_data['low_trend'] = reg[0] * ohlc_data['date_id'] + reg[1]
 
 
-    print(ohlc_data['low_trend'].head())
+    first_date = ohlc_data.index[0]
+    last_date = ohlc_data.index[-1]
 
-    ohlc_data['Adj Close'].plot()
-    ohlc_data['high_trend'].plot()
-    ohlc_data['low_trend'].plot()
-    plt.show()
+    high_trend = [(first_date, ohlc_data.at[first_date, 'high_trend']),
+                 (last_date, ohlc_data.at[last_date, 'high_trend'])]
+    
+    low_trend = [(first_date, ohlc_data.at[first_date, 'low_trend']),
+                 (last_date, ohlc_data.at[last_date, 'low_trend'])]
 
-    return ohlc_data
+    technicals_df = pd.DataFrame({"points":[high_trend, low_trend]})
+
+    return ohlc_data, technicals_df
 
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from DataExtract import retrieve_ticker_data
 
 from HoughTransform_ByHand import hough_trend_lines
-from LinearReg_Trendline import identify_trendlines_LinReg
+from LinearReg_Trendline import (identify_trendlines_LinReg, 
+                                 identify_lows_highs)
 
 from Visualize import visualize_ticker
 
@@ -9,9 +10,10 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 
-ohlc_data = retrieve_ticker_data("TSLA")
-# accumulated, ranked_lines = hough_trend_lines(ohlc_data["Low"])
+ohlc_data = retrieve_ticker_data("NIO")
+# accumulated, technicals_df = hough_trend_lines(ohlc_data["Low"])
+# visualize_ticker(ohlc_data, technicals_df)
 
-
-ohlc_data = identify_trendlines_LinReg(ohlc_data, dt.date(2020,1,1), dt.date(2020,8, 1))
-visualize_ticker(ohlc_data, ranked_lines)
+identify_lows_highs(ohlc_data, "High", 5, True)
+ohlc_data, technicals_df = identify_trendlines_LinReg(ohlc_data, dt.date(2020,9,30), dt.date(2020,12, 30))
+visualize_ticker(ohlc_data, technicals_df)
