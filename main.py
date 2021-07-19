ee16b9d416b7d132eb2368df3af740d4110b80e5
Egor <isakson98@gmail.com>
2021-07-19 15:13:55 -0400

    removed Hough files + updated TODO plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from DataExtract import retrieve_ticker_data
 
-from HoughTransform_ByHand import hough_trend_lines
 from LinearReg_Trendline import (identify_decending_trendlines_LinReg, 
                                  identify_lows_highs)
 
@@ -10,7 +9,7 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 
-ohlc_data = retrieve_ticker_data("BNTX")
-ohlc_data = identify_lows_highs(ohlc_data, "High", 10)
-ohlc_data, technicals_df = identify_decending_trendlines_LinReg(ohlc_data, min_days_out=20, precisesness=2)
+ohlc_data = retrieve_ticker_data("AMZN")
+ohlc_data = identify_lows_highs(ohlc_data, "High", 5)
+ohlc_data, technicals_df = identify_decending_trendlines_LinReg(ohlc_data, min_days_out=10, precisesness=2)
 visualize_ticker(ohlc_data, technicals_df)
